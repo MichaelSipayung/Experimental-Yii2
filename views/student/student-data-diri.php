2dcc5c6355df1187a8a755443c24a1c3fc5684f3
agustintampubolon <Michaelsipayung123@gmail.com>
2023-10-18 09:58:16 +0700

    add some improvements to data pribadi

--- views/student/student-data-diri.php
+++ views/student/student-data-diri.php
@@ -19,22 +19,88 @@ $title  = 'Data Diri Mahasiswa';
     <!-- display the label for nama on the left -->
     <?php echo $form->field($model_student_data_diri,'nama')->label('Nama'); ?>
     <!-- display the label for jenis_kelamin_id on the left -->
-    <?= $form->field($model_student_data_diri, 'jenis_kelamin_id')->dropDownList(['0' => 'Pria', '1' => 'Wanita']) ?>
-    <!-- display the label for tanggal_lahir on the left -->
+    <?php
+        $gen  = [ '0' => 'Pria', //gender map
+                  '1' => 'Wanita',];
+        echo $form->field($model_student_data_diri, 'jenis_kelamin_id')
+            ->dropDownList($gen, ['prompt' => 'Pilih Jenis Kelamin']);
+    ?>
 
     <?php echo $form->field($model_student_data_diri,'tanggal_lahir')->label('Tanggal Lahir'); ?>
     <!-- display the label for tempat_lahir on the left -->
     <?php echo $form->field($model_student_data_diri,'tempat_lahir')->label('Tempat Lahir'); ?>
     <!-- display the label for agama_id on the left -->
-    <?php echo $form->field($model_student_data_diri,'agama_id')->label('Agama'); ?>
+    <?php
+        $relegion = [
+            'Islam' => 'Islam',
+            'Kristen' => 'Kristen',
+            'Katolik' => 'Katolik',
+            'Hindu' => 'Hindu',
+            'Budha' => 'Budha',
+            'Konghucu' => 'Konghucu',
+        ];
+        echo $form->field($model_student_data_diri, 'agama_id')
+            ->dropDownList($relegion, ['prompt' => 'Pilih Agama']);
+    ?>
     <!-- display the label for alamat on the left -->
     <?php echo $form->field($model_student_data_diri,'alamat')->label('Alamat'); ?>
     <!-- display the label for kelurahan on the left -->
     <?php echo $form->field($model_student_data_diri,'kelurahan')->label('Kelurahan'); ?>
     <!-- display the label for provinsi on the left -->
-    <?php echo $form->field($model_student_data_diri,'provinsi')->label('Provinsi'); ?>
-    <!-- display the label for kabupaten on the left -->
-    <?php echo $form->field($model_student_data_diri,'kabupaten')->label('Kabupaten'); ?>
+    <!-- dropdown menu for all indonesian province -->
+    <?php
+    //map for all indonesia province, need more improvement like store in model
+    $provinces = [
+        'Aceh' => 'Aceh',
+        'Bali' => 'Bali',
+        'Bangka Belitung' => 'Bangka Belitung',
+        'Banten' => 'Banten',
+        'Bengkulu' => 'Bengkulu',
+        'Gorontalo' => 'Gorontalo',
+        'Jakarta' => 'Jakarta',
+        'Jambi' => 'Jambi',
+        'Jawa Barat' => 'Jawa Barat',
+        'Jawa Tengah' => 'Jawa Tengah',
+        'Jawa Timur' => 'Jawa Timur',
+        'Kalimantan Barat' => 'Kalimantan Barat',
+        'Kalimantan Selatan' => 'Kalimantan Selatan',
+        'Kalimantan Tengah' => 'Kalimantan Tengah',
+        'Kalimantan Timur' => 'Kalimantan Timur',
+        'Kalimantan Utara' => 'Kalimantan Utara',
+        'Kepulauan Riau' => 'Kepulauan Riau',
+        'Lampung' => 'Lampung',
+        'Maluku' => 'Maluku',
+        'Maluku Utara' => 'Maluku Utara',
+        'Nusa Tenggara Barat' => 'Nusa Tenggara Barat',
+        'Nusa Tenggara Timur' => 'Nusa Tenggara Timur',
+        'Papua' => 'Papua',
+        'Papua Barat' => 'Papua Barat',
+        'Riau' => 'Riau',
+        'Sulawesi Barat' => 'Sulawesi Barat',
+        'Sulawesi Selatan' => 'Sulawesi Selatan',
+        'Sulawesi Tengah' => 'Sulawesi Tengah',
+        'Sulawesi Tenggara' => 'Sulawesi Tenggara',
+        'Sulawesi Utara' => 'Sulawesi Utara',
+        'Sumatera Barat' => 'Sumatera Barat',
+        'Sumatera Selatan' => 'Sumatera Selatan',
+        'Sumatera Utara' => 'Sumatera Utara',
+        'Yogyakarta' => 'Yogyakarta',
+    ];
+    echo $form->field($model_student_data_diri, 'provinsi')
+        //fetch alll data in $provinces variable to dropdown menu
+        ->dropDownList($provinces, ['prompt' => 'Pilih Provinsi','id' => 'province-dropdown']);
+        //->dropDownList(['0' => 'Pria', '1' => 'Wanita'])
+    ?>
+    <?php
+        $cities = [ //need improvement like store in model
+        'Aceh' => ['Banda Aceh', 'Langsa', 'Lhokseumawe', 'Sabang', 'Subulussalam'],
+        'Bali' => ['Denpasar', 'Badung', 'Gianyar', 'Tabanan', 'Bangli'],
+        'Bangka Belitung' => ['Pangkal Pinang', 'Tanjung Pandan', 'Muntok', 'Toboali', 'Koba'],
+        // ... and so on for all provinces
+    ];
+    echo $form->field($model_student_data_diri, 'kabupaten')
+        ->dropDownList($cities, ['prompt' => 'Pilih Kabupaten/ Kota']);
+    ?>
     <!-- display the label for alamat_kec on the left -->
     <?php echo $form->field($model_student_data_diri,'alamat_kec')->label('Alamat Kecamatan'); ?>
     <!-- display the label for kode_pos on the left -->
