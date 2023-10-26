41477ad565ddbe9db7ec38a17d09e74ac83a592f
Michael Sipayung <michaelsipayung123@gmail.com>
2023-10-26 17:16:37 +0700

    push current state

--- views/student/register-student.php
+++ views/student/register-student.php
@@ -56,7 +56,7 @@ $this->title = 'Registrasi Akun Calon Mahasiswa Baru';
         outline: none;
     }
 </style>
-<br><br><br>
+<br><br><br>><br><br>
 <div class="row justify-content-center">
     <div class="col-md-6">
         <div class="card">
@@ -103,9 +103,9 @@ $this->title = 'Registrasi Akun Calon Mahasiswa Baru';
                 <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h5v-1a1.9 1.9 0 0 1 .01-.2 4.49 4.49 0 0 1 1.534-3.693C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4Zm7 0a1 1 0 0 1 1-1v-1a2 2 0 1 1 4 0v1a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-2Zm3-3a1 1 0 0 0-1 1v1h2v-1a1 1 0 0 0-1-1Z"/>
                 </svg></span>{input}</div>']
                 )->textInput(['maxlength'=>true, 'placeholder'=>'Phone Number'])->label(false) ?>
-<div class="form-group col-sm-12">
-    <?= Html::submitButton('Register', ['class' => 'btn btn-primary btn-block form-control']) ?>
-</div>
+                <div class="form-group col-sm-12">
+                    <?= Html::submitButton('Register', ['class' => 'btn btn-primary btn-block form-control']) ?>
+                </div>
                 <?php ActiveForm::end(); ?>
             </div>
         </div>
